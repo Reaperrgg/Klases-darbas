d4a7219c83e00f77e35793e6450b3aa2131c6771
Tomas Večkys <tomasveck@gmail.com>
2021-12-16 20:24:56 +0200

    clases darbai + zaidimas

--- images/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if(isset($_SESSION['arPrisijunges'])){
-    echo "matome slaptus duomenis:)";
-}else {
-    header("Location: http://localhost/loginform.php");
-}
--- images/login.php
+++
@@ -1,5 +0,0 @@
-<form action="login.php" method="post">
-     <input type="text" name="username" placeholder="Enter your username" required>
-     <input type="password" name="password" placeholder="Enter your pasword" required>
-     <input type="submit" value="Submit">
-</form>
