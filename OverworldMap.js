c625317a62801bb7038354ce45a811ac520f606c
Tomas Večkys <tomasveck@gmail.com>
2021-12-19 10:30:12 +0200

    zaidimas prideti npc

--- OverworldMap.js
+++ OverworldMap.js
@@ -22,28 +22,36 @@ window.OverworldMap = {
     DemoRoom: {
         lowerSrc: "/images/DemoLower.png",
         upperSrc: "/images/DemoUpper.png",
-        gameObject:{
+        gameObject: {
             hero: new GameObject({
                 x: 5,
                 y: 6,
             }),
             npc1: new GameObject({
                 x: 7,
-                y:9,
-                src:"/images/npc1.png",
+                y: 9,
+                src: "/images/npc1.png",
             })
         }
     },
     Kitchen: {
-        lowerSrc: "/images/DemoLower.png",
-        upperSrc: "/images/DemoUpper.png",
-        gameObject:{
+        lowerSrc: "/images/KitchenLower.png",
+        upperSrc: "/images/KitchenUpper.png",
+        gameObject: {
             hero: new GameObject({
-                x: 5,
-                y: 6,
+                x: 3,
+                y: 1,
             }),
-            npc1: new GameObject({
-                x: 7,
-                y:9,
-                src:"/images/npc1.png",
+            npcA: new GameObject({
+                x: 9,
+                y: 2,
+                src: "/images/npc2.png",
+            }),
+            npcB: new GameObject({
+                x: 10,
+                y: 4,
+                src: "/images/npc3.png",
+            })
+        }
+    },
 }
