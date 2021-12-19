958d10a895ebe874686b73d0c7f137d00b173bdf
Tomas Večkys <tomasveck@gmail.com>
2021-12-19 14:30:16 +0200

    zaidimas prideti npc

--- Overworld.js
+++ Overworld.js
@@ -3,30 +3,29 @@ class Overworld {
         this.element = config.element;
         this.canvas = this.element.querySelector(".game-canvas");
         this.ctx = this.canvas.getContext("2d");
+        this.map = null;
     }
+
+   startGameLoop(){
+        const step = () => {
+
+            this.map.drawLowerImage(this.ctx);
+            Object.values(this.map.gameObjects).forEach(object => {
+                object.sprite.draw(this.ctx);
+            })
+            this.map.drawUpperImage(this.ctx);
+
+            console.log("stepping!");
+            requestAnimationFrame(() =>{
+                step();
+        })
+       }
+       step();
+   }
+
+
     init() {
-
+        this.map = new OverworldMap(window.OverworldMaps.DemoRoom);
         this.startGameLoop();
-
-        const image = new Image();
-        image.onload = () => {
-            this.ctx.drawImage(image, 0, 0)
-        };
-        image.src = "/images/DemoLower.png";
-        //Place some game objects!
-        const hero = new GameObject({
-            x: 5,
-            y: 6,
-        })
-        const npc1 = new GameObject({
-            x: 7,
-            y: 9,
-            src: "/images/npc1.png"
-        })
-
-        setTimeout(() => {
-            hero.sprite.draw(this.ctx);
-            npc1.sprite.draw(this.ctx);
-        },200)
     }
 }
